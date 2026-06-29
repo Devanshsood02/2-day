0f1dce9ef27ed51bca111b791b7bb7b5ec0db708
Devansh Sood <sooddevansh143@gmail.com>
2026-06-29 23:17:48 +0530

    about page

--- server.js
+++ server.js
@@ -6,4 +6,8 @@ const app= express()
 app.get("/",(req,res)=>{
 res.send("Hello world")
 })
+
+app.get("/about",(req,res)=>{
+    res.send("this is about page")
+})
 app.listen(3000,console.log("Server is runnin on port 3000"))
